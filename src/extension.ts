5c6776629b0f77048087e18d3247ca1199f3c0fa
wszdw2009 <252675163@qq.com>
2018-05-01 18:56:07 +0800

    小demo

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,120 @@
 import * as vscode from "vscode";
+import {
+  Event,
+  EventEmitter,
+  ExtensionContext,
+  Range,
+  Selection,
+  SymbolInformation,
+  SymbolKind,
+  TextDocument,
+  TextEditor,
+  TextEditorRevealType,
+  TreeDataProvider,
+  TreeItem,
+  TreeItemCollapsibleState,
+  TreeView,
+  commands,
+  window,
+  workspace,
+  Position
+} from "vscode";
 
-import { SymbolOutlineProvider } from "./symbolOutline";
-
+import { getIcon } from "./icons";
+let path = require("path")
 export function activate(context: vscode.ExtensionContext) {
-  const symbolOutlineProvider = new SymbolOutlineProvider(context);
+  const symbolOutlineProvider = new Xb1(context);
+}
+class SymbolNode {
+  parent?: SymbolNode;
+  symbol: SymbolInformation;
+  children: SymbolNode[];
+
+  constructor(symbol?: SymbolInformation) {
+    this.children = [];
+    this.symbol = symbol;
+  }
+}
+class Xb1 {
+  symbolViewer: TreeView<SymbolNode>;
+  constructor(context: ExtensionContext) {
+    const treeDataProvider = new Xb1TreeDataProvider(context);
+    this.symbolViewer = window.createTreeView("xb1", {
+      treeDataProvider
+    });
+    commands.registerCommand("xb1.refresh", () => {
+      treeDataProvider.refresh();
+    });
+  }
+}
+export class Xb1TreeDataProvider
+  implements TreeDataProvider<SymbolNode> {
+  private _onDidChangeTreeData: EventEmitter<SymbolNode | null> = new EventEmitter<SymbolNode | null>();
+  readonly onDidChangeTreeData: Event<SymbolNode | null> = this
+    ._onDidChangeTreeData.event;
+    
+  private context: ExtensionContext;
+  private tree: SymbolNode;
+  private editor: TextEditor;
+
+  constructor(context: ExtensionContext) {
+    this.context = context;
+  }
+
+  private getSymbols(document: TextDocument): Thenable<SymbolInformation[]> {
+    return commands.executeCommand<SymbolInformation[]>(
+      "vscode.executeDocumentSymbolProvider",
+      document.uri
+    );
+  }
+
+
+  private async updateSymbols(editor: TextEditor): Promise<void> {
+    const a= new SymbolNode();
+    const tree = new SymbolNode();
+    this.editor = editor;
+    let c=new SymbolNode()
+    tree.children.push(c)
+    c.parent=tree;
+    tree.parent=a;
+    a.children.push(tree)
+    this.tree = a;
+  }
+
+  async getChildren(node?: SymbolNode): Promise<SymbolNode[]> {
+    if (node) {
+      return node.children;
+    } else {
+      await this.updateSymbols(window.activeTextEditor);
+      return this.tree ? this.tree.children : [];
+    }
+  }
+
+  getParent(node: SymbolNode): SymbolNode {
+    return node.parent;
+  }
+
+  getTreeItem(node: SymbolNode): TreeItem {
+    if(!node){
+      debugger
+    }
+    let treeItem = new TreeItem("试试");
+    if (node.children.length) {
+      treeItem.collapsibleState =TreeItemCollapsibleState.Expanded
+    } else {
+      treeItem.collapsibleState = TreeItemCollapsibleState.None;
+    }
+    treeItem.command = {
+      command: "xb1.revealRange",
+      title: "",
+      arguments: [this.editor]
+    };
+
+    treeItem.iconPath = getIcon(1, this.context);
+    return treeItem;
+  }
+
+  refresh() {
+    this._onDidChangeTreeData.fire();
+  }
 }
