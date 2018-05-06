c8b93d2446cf113235d01dd338dc609a3a5d9fa5
wszdw2009 <252675163@qq.com>
2018-05-06 18:47:03 +0800

    0.1.4 更新 命令第二次执行错误修改

--- xb1Extension.js
+++ xb1Extension.js
@@ -1,5 +1,5 @@
 class Node{
-    constructor(pk,label,type, detail,tooltip,parent) {
+    constructor({pk,label,type, detail,tooltip,parent}) {
     this.pk=pk
     this.label = label;
     this.type = type;
@@ -11,25 +11,24 @@ class Node{
     }
   }
 }
-let root=new Node("root","",0,"","",null)
-let docsNode=new Node("docsNode","查阅的文档",0,"","可以翻看项目中所需要用到的文档",root) 
-let docs1 =new Node("docs1","moment文档",1,"https://www.tslang.cn/docs/handbook/classes.html","moment文档2",docsNode) 
-// {
-//     [
-//         {
-//             label:"查阅的文档",
-//             title:"可以翻看项目中所需要用到的文档",
-//             type:0,
-//             detail:"",
-//             children:[
-//                 {
-//                     label:"moment文档",
-//                     title:"",
-//                     type:1,
-//                     detail:"https://www.tslang.cn/docs/handbook/classes.html"
-//                 }
-//             ]
-//         }
-//     ]
-// }
+let root=new Node({
+    pk:"root",
+    label:"",
+    type:0,
+    detail:"",
+    tooltip:"",
+    parent:null
+})
+let docsNode=new Node({
+    pk:"docsNode",label:"查阅的文档",type:0,detail:"",tooltip:"可以翻看项目中所需要用到的文档",parent:root
+}) 
+let docs1 =new Node({
+    pk:"docs1",label:"moment文档",type:1,detail:"https://www.tslang.cn/docs/handbook/classes.html",tooltip:"moment文档2",parent:docsNode
+}) 
+let packageJsonCMD= new Node({
+    pk:"packageJsonCMD",label:"package.json中的命令",type:0,detail:"",tooltip:"可以一览package.json中的命令",parent:root
+}) 
+let CMD1=new Node({
+    pk:"CMD1",label:"test",type:2,detail:"msbuild",tooltip:"测试啊",parent:packageJsonCMD
+}) 
 module.exports=root
